5873cf952c6baae172fc6b7cff2d14d9786330a3
Phạm Minh <113084584+Minhpche153232@users.noreply.github.com>
2024-08-07 11:55:24 +0700

    Add DBContext

---
+++ src/java/dao/DBContext.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author catmi
+ */
+public class DBContext {
+    Connection conn;
+    PreparedStatement ps;
+    ResultSet rs;
+    
+    private final String serverName = "localhost";
+    private final String dbName = "OnlineShop_SWP391";
+    private final String portNumber = "1433";
+    private final String userID = "sa";
+    private final String password = "123";
+
+    public DBContext() {
+        try {
+            String url = "jdbc:sqlserver://" + serverName + ":" + portNumber + ";databaseName=" + dbName;
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            conn = DriverManager.getConnection(url, userID, password);
+        } catch (Exception e) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, e);
+        }
+
+    }
+  
+}
