c185718a2815048ad499617c68b4fa6ba6c7e439
tuanddhe151088 <tuanddhe151088@fpt.edu.vn>
2024-08-17 18:44:02 +0700

    fix some thing

--- src/java/controller/ShopController.java
+++ src/java/controller/ShopController.java
@@ -4,34 +4,33 @@
  */
 package controller;
 
-import dao.ProductDAO;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.util.List;
-import models.ProductDetail;
 
 /**
  *
  * @author Admin
  */
-public class HomeController extends HttpServlet {
+public class ShopController extends HttpServlet {
 
-    
- 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        ProductDAO productDAO = new ProductDAO();
-//        List<ProductDetail> listCheapest = productDAO.getTopCheapestProduct();
-//        request.setAttribute("listCheapest", listCheapest);
-        request.getRequestDispatcher("home.jsp").forward(request, response);
+request.getRequestDispatcher("shop.jsp").forward(request, response);
     }
 
-   
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
